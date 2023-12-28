85d6d657e4303cc6109d9e50df66c4b3600c32a6
meltjacTechs <ebwire02@gmail.com>
2023-12-28 15:00:36 +0300

    Error corrected

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -2,7 +2,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -13,9 +12,14 @@ import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/validation";
 import { z } from "zod";
+import Loader from "@/components/ui/shared/Loader";
 
 
 const SignupForm = () => {
+  const isLoading = false;
+
+
+
     // 1. Define your form.
     const form = useForm<z.infer<typeof SignupValidation>>({
       resolver: zodResolver(SignupValidation),
@@ -39,12 +43,25 @@ const SignupForm = () => {
       <div className="sm:w-420 flex-center flex-col">
         < img src="/assets/images/logo.svg" alt="logo" />
 
-        <h2 className="h3-bold md:h3-bold pt-5 sm:pt-5">Create a new account</h2>
+        <h2 className="h3-bold md:h3-bold pt-6 sm:pt-12">Create a new account</h2>
         <p className="text-light-3 small-medium md:base-regular">
-          Ready to buzz🐝 into the Hive?<br/> Unlock your 
-          sweet spot🍯and<br/> connect with the swarm!💯</p>
-      </div>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          Ready to buzz🐝 into the Hive? Unlock<br/>your 
+          sweet spot🍯and connect with <br/>the swarm!💯</p>
+      
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-input" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <FormField
           control={form.control}
           name="username"
@@ -52,17 +69,47 @@ const SignupForm = () => {
             <FormItem>
               <FormLabel>Username</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input type="text" className="shad-input" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" className="shad-input" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" className="shad-input" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="shad-button_primary">
+          { isLoading ? (
+            <div className="flex-center gap-2">
+              <Loader />Loading...
+            </div>
+          ): "Sign Up"}
+        </Button>
       </form>
+      </div>
     </Form>
   );
 }
